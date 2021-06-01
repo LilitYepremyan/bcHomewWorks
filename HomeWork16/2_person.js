04ce51d25c0fec4212c20a586ed5c9aa3a11af99
Lilit Yepremyan <lilit.epremyan.96@gmail.com>
2021-06-01 14:55:30 +0400

    add new task

--- HomeWork16/2_person.js
+++ HomeWork16/2_person.js
@@ -1,8 +1,3 @@
-// Create a method in the Person class which returns how another person's age compares. Given the instances
-// p1, p2 and p3, which will be initialised with the attributes name and age,return a sentence in the following
-// format:
-// {other person name} is {olderthan / youngerthan / the same age as} me.
-
 class Person {
   constructor(name, age) {
     this.name = name;
@@ -20,6 +15,6 @@ class Person {
 const p1 = new Person("Samuel", 24);
 const p2 = new Person("Joel", 36);
 const p3 = new Person("Lily", 24);
-// console.log(p1.compareAge(p2)); //  "Joel is older than me."
-// console.log( p2.compareAge(p1))// "Samuel is younger than me."
-console.log( p1.compareAge(p3)) // "Lily is the same age as me."
+console.log(p1.compareAge(p2)); //  "Joel is older than me."
+console.log(p2.compareAge(p1)); // "Samuel is younger than me."
+console.log(p1.compareAge(p3)); // "Lily is the same age as me."
